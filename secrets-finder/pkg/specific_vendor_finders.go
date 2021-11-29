0052155bc04d042f33bedfa36355c61df10d5e36
Adedayo Adetoye <aadetoye@mimecast.com>
2021-11-29 14:26:15 +0000

    Added Gitlab personal access token vendor detection

--- secrets-finder/pkg/specific_vendor_finders.go
+++ secrets-finder/pkg/specific_vendor_finders.go
@@ -15,8 +15,12 @@ Higher recall/confidence patterns from vendors and other well-known secret patte
 var (
 	//https://github.blog/2021-04-05-behind-githubs-new-authentication-token-formats/
 	//https://github.blog/changelog/2021-03-31-authentication-token-format-updates-are-generally-available/
-	githubRegex     = fmt.Sprintf(`(?i:(gh[pousr]_[A-Za-z0-9_]{%d,255}))`, 36) // Current min is 40 chars hence 36 = 40-len(gho_),
+	githubRegex     = fmt.Sprintf(`(?i:(gh[pousr]_[A-Za-z0-9_]{%d,255}))`, 40) // Current min is 40 chars
 	descGithubToken = "GitHub Authentication Token"
+
+	//https://about.gitlab.com/releases/2021/11/22/gitlab-14-5-released/#new-gitlab-access-token-prefix-and-detection
+	gitlabRegex     = fmt.Sprintf(`(?i:(glpat-[A-Za-z0-9\-]{%d,255}))`, 20) // Current min is 20 chars
+	descGitlabToken = "GitLab Personal Access Token"
 
 	//https://api.slack.com/authentication/token-types
 	slackRegex     = fmt.Sprintf(`(?i:(xox[apbr]-2?[A-Za-z0-9-]{%d,}))`, 24) //examples here https://api.slack.com/authentication/basics longer than 24 chars,
@@ -86,6 +90,7 @@ var (
 
 	vendorSecretPatterns = map[string]string{
 		descGithubToken:      githubRegex,
+		descGitlabToken:      gitlabRegex,
 		descSlackToken:       slackRegex,
 		descStripeToken:      stripeRegex,
 		descGoCardlessToken:  goCardlessRegex,
